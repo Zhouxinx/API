578c57e368c7f3835bac61c549bdbb70fc300776
Zhouxinx <36874133+Zhouxinx@users.noreply.github.com>
2019-03-29 21:55:04 +0800

    Create python_repos_01.py

---
+++ python_repos_01.py
@@ -0,0 +1,21 @@
+import requests
+
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print('Status code :' , r.status_code)
+
+#将API响应存储在一个变量之中
+response_dict = r.json()
+
+#处理结果
+print('Total repositories :' , response_dict['total_count'])
+
+#搜索有关仓库的信息
+repo_dicts = response_dict['items']
+print('repositories returned: ' , len(repo_dicts))
+
+#研究第一个仓库
+repo_dict = repo_dicts[0]
+print("\nkeys : ", len(repo_dict))
+for key in sorted(repo_dict.keys()):
+	print(key)
